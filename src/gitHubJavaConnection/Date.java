8e5d722c16e6d1d99da2083297e0e52f97c2ed3a
dimitar.ivanov <dimitarivanov523@gmail.com>
2020-10-28 17:50:23 +0200

    Getter and the setters

--- src/gitHubJavaConnection/Date.java
+++ src/gitHubJavaConnection/Date.java
@@ -1,5 +1,84 @@
 package gitHubJavaConnection;
 
 public class Date {
+	
+	int sec;
+	int min;
+	int hour;
+	int day;
+	int month;
+	int year;
+	
+	
+	public int getSec() {
+		return sec;
+	}
+	
+	public void setSec(int _sec) {
+		sec = _sec;
+	}
+	
+	
+	public int getMin() {
+		return min;
+	}
+	
+	public void setMin(int _min) {
+		min = _min;
+	}
+	
+	
+	
+	public int getHour() {
+		return hour;
+	}
+	
+	public void setHour(int _hour) {
+		hour = _hour;
+	}
+	
+	
+	
+	
+	public int getDay() {
+		return day;
+	}
+	
+	public void setDay(int _day) {
+		day = _day;
+	}
+	
+	
+	
+	
+	public int getMonth() {
+		return month;
+	}
+	
+	public void setMonth(int _month) {
+		month = _month;
+	}
+	
+	
+	
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int _Year) {
+		year = _Year;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
